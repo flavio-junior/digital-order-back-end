de8be3b3f5936fbcd3b9575fab998d300df42e0e
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2024-12-29 06:44:33 -0300

    refactor: remove import unnecessary

--- back-end/src/main/kotlin/br/com/dashboard/company/controller/OrderController.kt
+++ back-end/src/main/kotlin/br/com/dashboard/company/controller/OrderController.kt
@@ -11,7 +11,6 @@ import br.com.dashboard.company.vo.order.CloseOrderRequestVO
 import br.com.dashboard.company.vo.order.OrderRequestVO
 import br.com.dashboard.company.vo.order.OrderResponseVO
 import br.com.dashboard.company.vo.order.UpdateStatusDeliveryRequestVO
-import br.com.dashboard.company.vo.reservation.ReservationRequestVO
 import br.com.dashboard.company.vo.reservation.ReservationResponseVO
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.ArraySchema
@@ -26,15 +25,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.net.URI
 
