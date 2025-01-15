03d60f0f5c04830a61c804d44cc06447b0d70abe
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-01-15 07:46:04 -0300

    refactor: add name correct

--- back-end/src/main/kotlin/br/com/dashboard/company/vo/payment/PaymentResponseVO.kt
+++ back-end/src/main/kotlin/br/com/dashboard/company/vo/payment/PaymentResponseVO.kt
@@ -2,6 +2,7 @@ package br.com.dashboard.company.vo.payment
 
 import br.com.dashboard.company.utils.common.PaymentType
 import br.com.dashboard.company.utils.common.TypeOrder
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Column
 
 data class PaymentResponseVO(
@@ -9,12 +10,12 @@ data class PaymentResponseVO(
     var date: String? = null,
     var hour: String? = null,
     var code: Long? = null,
-    @Column(name = "type_order", nullable = false)
+    @JsonProperty(value = "type_order")
     var typeOrder: TypeOrder? = null,
-    @Column(name = "type_payment", nullable = false)
+    @JsonProperty(value = "type_payment")
     var typePayment: PaymentType? = null,
     var discount: Boolean? = null,
-    @Column(name = "value_discount", nullable = true)
+    @JsonProperty(value = "value_discount")
     var valueDiscount: Double? = null,
     var total: Double = 0.0
 )
